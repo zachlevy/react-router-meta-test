22bfa6e8a727bbb0d2b2451165831ed569ea9064
Zachary Levy <zacharyalevy@gmail.com>
2017-11-15 00:54:54 -0500

    cleanup, bugfix missing directory

--- build-static-meta-routes.js
+++ build-static-meta-routes.js
@@ -1,10 +1,22 @@
 console.log("configure-static-routes");
 
 var fs = require('fs');
+var path = require('path')
 var replace = require('stream-replace')
 var routes = require('./config/static-meta-routes')
 
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
 routes.forEach(function(route, index) {
+  // ensureDirectoryExistence('build' + route.url)
+  fs.mkdirSync('build' + route.url)
   fs.createReadStream('build/index.html').pipe(
     replace('</head>', route.metaHtml + '</head>')
   ).pipe(
