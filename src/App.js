71edd55f459168587c0e5addf0a744652e82c4ee
Zachary Levy <zacharyalevy@gmail.com>
2017-11-14 22:14:11 -0500

    react router test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Test from './Test'
 
 class App extends Component {
   render() {
@@ -13,6 +16,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/test/' component={Test}/>
+        </Switch>
       </div>
     );
   }
