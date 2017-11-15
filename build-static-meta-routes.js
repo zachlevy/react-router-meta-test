676c3c9567e8a2e2c30080775ec428f13da945b4
Zachary Levy <zacharyalevy@gmail.com>
2017-11-15 14:19:32 -0500

    array of meta tags

--- build-static-meta-routes.js
+++ build-static-meta-routes.js
@@ -5,20 +5,10 @@ var path = require('path')
 var replace = require('stream-replace')
 var routes = require('./config/static-meta-routes')
 
-function ensureDirectoryExistence(filePath) {
-  var dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return true;
-  }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
-}
-
 routes.forEach(function(route, index) {
-  // ensureDirectoryExistence('build' + route.url)
   fs.mkdirSync('build' + route.url)
   fs.createReadStream('build/index.html').pipe(
-    replace('</head>', route.metaHtml + '</head>')
+    replace('</head>', route.metaTags.join() + '</head>')
   ).pipe(
     fs.createWriteStream(
       'build' + route.url + 'index.html'
--- config/static-meta-routes.js
+++ config/static-meta-routes.js
@@ -1,12 +1,14 @@
 var routes = [
   {
     url: "/test-meta/",
-    metaHtml: '<meta property="og:title" content="Test Static | React Router Meta Test" />\
-      <meta property="og:type" content="website" />\
-      <meta property="og:url" content="http://react-router-meta-test.s3-website-us-east-1.amazonaws.com/test-meta/" />\
-      <meta property="og:image" content="https://placehold.it/400x400?text=test" />\
-      <meta property="og:image:width" content="400" />\
-      <meta property="og:image:height" content="400" />'
+    metaTags: [
+      '<meta property="og:title" content="Test Static | React Router Meta Test" />',
+      '<meta property="og:type" content="website" />',
+      '<meta property="og:url" content="http://react-router-meta-test.s3-website-us-east-1.amazonaws.com/test-meta/" />',
+      '<meta property="og:image" content="https://placehold.it/400x400?text=test" />',
+      '<meta property="og:image:width" content="400" />',
+      '<meta property="og:image:height" content="400" />'
+    ]
   }
 ]
 module.exports = routes
